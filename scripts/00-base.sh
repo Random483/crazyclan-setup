13e3925f0d049d5cac66b66e32ebca526d6a09ef
Random483 <a.ross483@gmail.com>
2026-01-29 16:05:55 +0000

    removed cert

--- scripts/00-base.sh
+++ scripts/00-base.sh
@@ -80,9 +80,9 @@ ufw allow OpenSSH
 ufw --force enable
 
 # -------------------------------------------------------------------
-echo "==> Installing FreeIPA CA certificate (system-wide trust)"
-curl -fsSL -o /usr/local/share/ca-certificates/ipa-ca.crt https://ipa.crazyclan.lan/ipa/config/ca.crt
-update-ca-certificates
+# echo "==> Installing FreeIPA CA certificate (system-wide trust)"
+# curl -fsSL -o /usr/local/share/ca-certificates/ipa-ca.crt https://ipa.crazyclan.lan/ipa/config/ca.crt
+# update-ca-certificates
 
 # -------------------------------------------------------------------
 echo "==> Configuring timezone and locale"
